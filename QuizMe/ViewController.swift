d600c094b90abbf7d8c80c5ce64a34460adaec68
shaikha-305 <shaikhaaljenaidel@gmail.com>
2021-04-30 01:30:23 +0300

    Initial Commit

---
+++ QuizMe/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  QuizMe
+//
+//  Created by shaikha aljenaidel on 30/04/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
